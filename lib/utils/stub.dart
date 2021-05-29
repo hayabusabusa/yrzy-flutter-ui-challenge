b84dc70c583057c1457ad75b87086097ed40151a
hayabusabusa <hayabusabusaSY@gmail.com>
2021-05-29 23:57:27 +0900

    ui: add stub class

---
+++ lib/utils/stub.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// 画面表示用のデータをスタブとして返すクラス
+class Stub {
+  /// 色選択画面用の色一覧
+  static List<Color> get colors => [
+    Colors.red,
+    Colors.yellow,
+    Colors.orange,
+    Colors.green,
+    Colors.blue,
+  ];
+}
