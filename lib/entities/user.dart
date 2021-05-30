39d9cc927a6b239f2e5bbc5063a76e3a8d5b92b8
hayabusabusa <hayabusabusaSY@gmail.com>
2021-05-30 21:27:08 +0900

    ui: add user class and update stub class

---
+++ lib/entities/user.dart
@@ -0,0 +1,11 @@
+class User {
+  final String name;
+  final String avatarURL;
+  final String message;
+
+  User({
+    required this.name,
+    required this.avatarURL,
+    required this.message,
+  });
+}
--- lib/utils/stub.dart
+++ lib/utils/stub.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:ui_challenge/entities/entities.dart';
+
 /// 画面表示用のデータをスタブとして返すクラス
 class Stub {
   /// 色選択画面用の色一覧
@@ -9,5 +11,13 @@ class Stub {
     Colors.green.shade200,
     Colors.blue.shade200,
     Colors.indigo.shade200,
+  ];
+
+  /// メッセージ一覧画面用のユーザーデータ一覧
+  static List<User> get users => [
+    User(name: 'hayabusa', avatarURL: 'https://avatars.githubusercontent.com/u/31949692?s=64&v=4', message: 'サッカーやりたいね。たくあん刻みながら。'),
+    User(name: 'totty', avatarURL: 'https://avatars.githubusercontent.com/u/31689275?s=64&v=4', message: 'いただきます...っと'),
+    User(name: 'Kbayashi', avatarURL: 'https://avatars.githubusercontent.com/u/82300193?s=64&v=4', message: 'モンハンいつやる？'),
+    User(name: 'dev-tky', avatarURL: 'https://avatars.githubusercontent.com/u/69100759?s=88&v=4', message: 'ご不便をおかけしております。'),
   ];
 }
